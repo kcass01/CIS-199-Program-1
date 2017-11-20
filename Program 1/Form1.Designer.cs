6619c46d432b3f8031d6bdff16de2af3776702bf
kcass01 <33797171+kcass01@users.noreply.github.com>
2017-11-20 09:47:12 -0500

    Initial commit

---
+++ Program 1/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Program_1
+{
+    partial class program1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.totalPaintAreaOutputLabel = new System.Windows.Forms.Label();
+            this.paintRequiredOutputLabel = new System.Windows.Forms.Label();
+            this.paintCostOutputLabel = new System.Windows.Forms.Label();
+            this.hoursRequiredOutputLabel = new System.Windows.Forms.Label();
+            this.laborCostOutputLabel = new System.Windows.Forms.Label();
+            this.totalCostOutputLabel = new System.Windows.Forms.Label();
+            this.totalPaintAreaLabel = new System.Windows.Forms.Label();
+            this.paintRequiredLabel = new System.Windows.Forms.Label();
+            this.hoursRequiredLabel = new System.Windows.Forms.Label();
+            this.paintCostLabel = new System.Windows.Forms.Label();
+            this.laborCostLabel = new System.Windows.Forms.Label();
+            this.totalCostLabel = new System.Windows.Forms.Label();
+            this.squareFeetLabel = new System.Windows.Forms.Label();
+            this.coatsDesiredLabel = new System.Windows.Forms.Label();
+            this.paintPriceLabel = new System.Windows.Forms.Label();
+            this.squareFeetTextBox = new System.Windows.Forms.TextBox();
+            this.coatsDesiredTextBox = new System.Windows.Forms.TextBox();
+            this.paintPriceTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // totalPaintAreaOutputLabel
+            // 
+            this.totalPaintAreaOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalPaintAreaOutputLabel.Location = new System.Drawing.Point(524, 48);
+            this.totalPaintAreaOutputLabel.Name = "totalPaintAreaOutputLabel";
+            this.totalPaintAreaOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalPaintAreaOutputLabel.TabIndex = 0;
+            this.totalPaintAreaOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paintRequiredOutputLabel
+            // 
+            this.paintRequiredOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paintRequiredOutputLabel.Location = new System.Drawing.Point(524, 91);
+            this.paintRequiredOutputLabel.Name = "paintRequiredOutputLabel";
+            this.paintRequiredOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.paintRequiredOutputLabel.TabIndex = 1;
+            this.paintRequiredOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paintCostOutputLabel
+            // 
+            this.paintCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paintCostOutputLabel.Location = new System.Drawing.Point(524, 177);
+            this.paintCostOutputLabel.Name = "paintCostOutputLabel";
+            this.paintCostOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.paintCostOutputLabel.TabIndex = 2;
+            this.paintCostOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hoursRequiredOutputLabel
+            // 
+            this.hoursRequiredOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hoursRequiredOutputLabel.Location = new System.Drawing.Point(524, 134);
+            this.hoursRequiredOutputLabel.Name = "hoursRequiredOutputLabel";
+            this.hoursRequiredOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.hoursRequiredOutputLabel.TabIndex = 2;
+            this.hoursRequiredOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // laborCostOutputLabel
+            // 
+            this.laborCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.laborCostOutputLabel.Location = new System.Drawing.Point(524, 220);
+            this.laborCostOutputLabel.Name = "laborCostOutputLabel";
+            this.laborCostOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.laborCostOutputLabel.TabIndex = 3;
+            this.laborCostOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalCostOutputLabel
+            // 
+            this.totalCostOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalCostOutputLabel.Location = new System.Drawing.Point(524, 263);
+            this.totalCostOutputLabel.Name = "totalCostOutputLabel";
+            this.totalCostOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalCostOutputLabel.TabIndex = 4;
+            this.totalCostOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalPaintAreaLabel
+            // 
+            this.totalPaintAreaLabel.AutoSize = true;
+            this.totalPaintAreaLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalPaintAreaLabel.Location = new System.Drawing.Point(324, 48);
+            this.totalPaintAreaLabel.Name = "totalPaintAreaLabel";
+            this.totalPaintAreaLabel.Size = new System.Drawing.Size(194, 17);
+            this.totalPaintAreaLabel.TabIndex = 5;
+            this.totalPaintAreaLabel.Text = "Total Square Feet to be Painted:";
+            // 
+            // paintRequiredLabel
+            // 
+            this.paintRequiredLabel.AutoSize = true;
+            this.paintRequiredLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paintRequiredLabel.Location = new System.Drawing.Point(359, 91);
+            this.paintRequiredLabel.Name = "paintRequiredLabel";
+            this.paintRequiredLabel.Size = new System.Drawing.Size(159, 17);
+            this.paintRequiredLabel.TabIndex = 6;
+            this.paintRequiredLabel.Text = "Gallons of Paint Required:";
+            // 
+            // hoursRequiredLabel
+            // 
+            this.hoursRequiredLabel.AutoSize = true;
+            this.hoursRequiredLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursRequiredLabel.Location = new System.Drawing.Point(361, 134);
+            this.hoursRequiredLabel.Name = "hoursRequiredLabel";
+            this.hoursRequiredLabel.Size = new System.Drawing.Size(157, 17);
+            this.hoursRequiredLabel.TabIndex = 7;
+            this.hoursRequiredLabel.Text = "Hours of Labor Required:";
+            // 
+            // paintCostLabel
+            // 
+            this.paintCostLabel.AutoSize = true;
+            this.paintCostLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paintCostLabel.Location = new System.Drawing.Point(411, 177);
+            this.paintCostLabel.Name = "paintCostLabel";
+            this.paintCostLabel.Size = new System.Drawing.Size(107, 17);
+            this.paintCostLabel.TabIndex = 8;
+            this.paintCostLabel.Text = "Cost of the Paint:";
+            // 
+            // laborCostLabel
+            // 
+            this.laborCostLabel.AutoSize = true;
+            this.laborCostLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.laborCostLabel.Location = new System.Drawing.Point(427, 220);
+            this.laborCostLabel.Name = "laborCostLabel";
+            this.laborCostLabel.Size = new System.Drawing.Size(91, 17);
+            this.laborCostLabel.TabIndex = 9;
+            this.laborCostLabel.Text = "Cost of Labor:";
+            // 
+            // totalCostLabel
+            // 
+            this.totalCostLabel.AutoSize = true;
+            this.totalCostLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalCostLabel.Location = new System.Drawing.Point(443, 263);
+            this.totalCostLabel.Name = "totalCostLabel";
+            this.totalCostLabel.Size = new System.Drawing.Size(75, 17);
+            this.totalCostLabel.TabIndex = 10;
+            this.totalCostLabel.Text = "Total Costs:";
+            // 
+            // squareFeetLabel
+            // 
+            this.squareFeetLabel.AutoSize = true;
+            this.squareFeetLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.squareFeetLabel.Location = new System.Drawing.Point(12, 46);
+            this.squareFeetLabel.Name = "squareFeetLabel";
+            this.squareFeetLabel.Size = new System.Drawing.Size(238, 17);
+            this.squareFeetLabel.TabIndex = 11;
+            this.squareFeetLabel.Text = "Total Square Feet of Wall to be Painted:";
+            // 
+            // coatsDesiredLabel
+            // 
+            this.coatsDesiredLabel.AutoSize = true;
+            this.coatsDesiredLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coatsDesiredLabel.Location = new System.Drawing.Point(12, 132);
+            this.coatsDesiredLabel.Name = "coatsDesiredLabel";
+            this.coatsDesiredLabel.Size = new System.Drawing.Size(209, 17);
+            this.coatsDesiredLabel.TabIndex = 12;
+            this.coatsDesiredLabel.Text = "Number of Coats of Paint Desired:";
+            // 
+            // paintPriceLabel
+            // 
+            this.paintPriceLabel.AutoSize = true;
+            this.paintPriceLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paintPriceLabel.Location = new System.Drawing.Point(12, 218);
+            this.paintPriceLabel.Name = "paintPriceLabel";
+            this.paintPriceLabel.Size = new System.Drawing.Size(151, 17);
+            this.paintPriceLabel.TabIndex = 13;
+            this.paintPriceLabel.Text = "Price of Paint Per Gallon:";
+            // 
+            // squareFeetTextBox
+            // 
+            this.squareFeetTextBox.Location = new System.Drawing.Point(15, 88);
+            this.squareFeetTextBox.Name = "squareFeetTextBox";
+            this.squareFeetTextBox.Size = new System.Drawing.Size(100, 20);
+            this.squareFeetTextBox.TabIndex = 14;
+            // 
+            // coatsDesiredTextBox
+            // 
+            this.coatsDesiredTextBox.Location = new System.Drawing.Point(15, 180);
+            this.coatsDesiredTextBox.Name = "coatsDesiredTextBox";
+            this.coatsDesiredTextBox.Size = new System.Drawing.Size(100, 20);
+            this.coatsDesiredTextBox.TabIndex = 15;
+            // 
+            // paintPriceTextBox
+            // 
+            this.paintPriceTextBox.Location = new System.Drawing.Point(15, 266);
+            this.paintPriceTextBox.Name = "paintPriceTextBox";
+            this.paintPriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.paintPriceTextBox.TabIndex = 16;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(226, 266);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 17;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.clearButton.Location = new System.Drawing.Point(327, 266);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 18;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // program1Form
+            // 
+            this.AcceptButton = this.calculateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.clearButton;
+            this.ClientSize = new System.Drawing.Size(661, 321);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.paintPriceTextBox);
+            this.Controls.Add(this.coatsDesiredTextBox);
+            this.Controls.Add(this.squareFeetTextBox);
+            this.Controls.Add(this.paintPriceLabel);
+            this.Controls.Add(this.coatsDesiredLabel);
+            this.Controls.Add(this.squareFeetLabel);
+            this.Controls.Add(this.totalCostLabel);
+            this.Controls.Add(this.laborCostLabel);
+            this.Controls.Add(this.paintCostLabel);
+            this.Controls.Add(this.hoursRequiredLabel);
+            this.Controls.Add(this.paintRequiredLabel);
+            this.Controls.Add(this.totalPaintAreaLabel);
+            this.Controls.Add(this.totalCostOutputLabel);
+            this.Controls.Add(this.laborCostOutputLabel);
+            this.Controls.Add(this.hoursRequiredOutputLabel);
+            this.Controls.Add(this.paintCostOutputLabel);
+            this.Controls.Add(this.paintRequiredOutputLabel);
+            this.Controls.Add(this.totalPaintAreaOutputLabel);
+            this.Name = "program1Form";
+            this.Text = "Program 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label totalPaintAreaOutputLabel;
+        private System.Windows.Forms.Label paintRequiredOutputLabel;
+        private System.Windows.Forms.Label paintCostOutputLabel;
+        private System.Windows.Forms.Label hoursRequiredOutputLabel;
+        private System.Windows.Forms.Label laborCostOutputLabel;
+        private System.Windows.Forms.Label totalCostOutputLabel;
+        private System.Windows.Forms.Label totalPaintAreaLabel;
+        private System.Windows.Forms.Label paintRequiredLabel;
+        private System.Windows.Forms.Label hoursRequiredLabel;
+        private System.Windows.Forms.Label paintCostLabel;
+        private System.Windows.Forms.Label laborCostLabel;
+        private System.Windows.Forms.Label totalCostLabel;
+        private System.Windows.Forms.Label squareFeetLabel;
+        private System.Windows.Forms.Label coatsDesiredLabel;
+        private System.Windows.Forms.Label paintPriceLabel;
+        private System.Windows.Forms.TextBox squareFeetTextBox;
+        private System.Windows.Forms.TextBox coatsDesiredTextBox;
+        private System.Windows.Forms.TextBox paintPriceTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Program 1/Form1.cs
@@ -0,0 +1,82 @@
+﻿//Grading ID: A5196
+//Program Number: Program 1
+//Due Date: 14 February 2017
+//Course Section: CIS 199-01
+//This program will caulcate the relevant costs with a painting project, given some inputs.
+//The user will input their wall space, their desired number of coats, and the cost of paint.
+//The program will output the total square feet to be done, the gallons of paint required,
+//the the hours of labor needed, the paint cost, the labor cost, and the total costs of the project.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_1
+{
+    public partial class program1Form : Form
+    {
+        private const int SQUARE_FOOT_PER_GALLON = 330; //This constant is the square footage from one gallon of paint
+        private const int HOURS_PER_GALLON = 6; //This constant is the hours to use paint one gallon
+        private const double COST_PER_HOUR = 10.50; //This constant is the price of labor per hour
+        public program1Form()
+        {
+            InitializeComponent();
+        }
+        
+        //This event is what occurs when the Calculate button is clicked. It will calculate the desired outputs.
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Inputs
+            int coatsOfPaint; //This variable is the number of coats of paint desired
+            coatsOfPaint = int.Parse(coatsDesiredTextBox.Text);
+            double wallSpace; //This variable is the wall space that will be painted
+            wallSpace = float.Parse(squareFeetTextBox.Text);
+            double paintPrice; //This is the paint price (per Gallon)
+            paintPrice = float.Parse(paintPriceTextBox.Text);
+            //Calculations
+            double totalPaintArea;//This variable is the total area to be painted, including all coats
+            totalPaintArea = wallSpace * coatsOfPaint;
+            double gallonsRequired;//This is the how much paint that will need to be bought (gallons)
+            gallonsRequired = totalPaintArea / SQUARE_FOOT_PER_GALLON;
+            double gallonsRequiredRounded;//This is the how many gallons, rounded up to an amount that is actually purchasable.
+            gallonsRequiredRounded = Math.Ceiling(gallonsRequired);
+            double hoursRequired; //This variable is the hours of labor that will be required
+            hoursRequired = (gallonsRequired) * HOURS_PER_GALLON;
+            double paintCost;//This is the cost of all gallons of paint
+            paintCost = gallonsRequiredRounded * paintPrice;
+            double laborCost;//This is the total cost of labor
+            laborCost = hoursRequired * COST_PER_HOUR;
+            double totalCost;//This is the total cost of paint and labor
+            totalCost = paintCost + laborCost;
+            //Outputs
+            totalPaintAreaOutputLabel.Text = totalPaintArea.ToString("n1");//This sets the total paint area output label
+            paintRequiredOutputLabel.Text = gallonsRequiredRounded.ToString("n0");//This sets the gallons of pant required output label
+            hoursRequiredOutputLabel.Text = hoursRequired.ToString("n1");//this sets the hours of labor output label
+            paintCostOutputLabel.Text = paintCost.ToString("c2");//This sets the paint cost output label
+            laborCostOutputLabel.Text = laborCost.ToString("c2");//This set the labor cost output label
+            totalCostOutputLabel.Text = totalCost.ToString("c2");//This sets the total cost output label
+
+        }
+
+        //This even serves as a clear button to reset the form should any mistakes be made by the user.
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Clear inputs
+            squareFeetTextBox.Text = "";
+            coatsDesiredTextBox.Text = "";
+            paintPriceTextBox.Text = "";
+            //Clear Outputs
+            totalPaintAreaOutputLabel.Text = "";
+            totalCostOutputLabel.Text = "";
+            paintRequiredOutputLabel.Text = "";
+            hoursRequiredOutputLabel.Text = "";
+            laborCostOutputLabel.Text = "";
+            paintCostOutputLabel.Text = "";
+        }
+    }
+}
